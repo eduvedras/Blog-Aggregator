fd0d2e76eea09fd929e7a1ef0bf3493a2ac984bb
eduvedras <edu.vedras@gmail.com>
2024-08-10 15:39:11 +0100

    Connect to db and create user handler

--- main.go
+++ main.go
@@ -1,25 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/eduvedras/Blog-Aggregator/internal/database"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main(){
 	err := godotenv.Load()
 	if err != nil{
-		log.Fatal("Failed to load environment variables.")
+		log.Fatal(err)
 	}
 
 	port := os.Getenv("PORT")
+	dbURL := os.Getenv("CONN_STRING")
+	
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	cfg := apiConfig{
+		DB: database.New(db),
+	}
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /v1/healthz", handlerReadiness)
 	mux.HandleFunc("GET /v1/err", handlerErr)
+	mux.HandleFunc("POST /v1/users", cfg.handlerCreateUser)
 
 	server := &http.Server{
 		Addr: ":" + port,
---
+++ users.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/eduvedras/Blog-Aggregator/internal/database"
+	"github.com/google/uuid"
+)
+
+func (cfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request){
+	defer r.Body.Close()
+
+	decoder := json.NewDecoder(r.Body)
+
+	params := struct{
+		Name string `json:"name"`
+	}{}
+	err := decoder.Decode(&params)
+	if err != nil{
+		respondWithError(w, http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
+		return
+	}
+
+	user, err := cfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID: uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name: params.Name,
+	})
+	if err != nil{
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create user")
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, user)
+}
