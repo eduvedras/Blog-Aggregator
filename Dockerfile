21b701b20e383deb130f352e63c9fc8f58d45e0c
eduvedras <edu.vedras@gmail.com>
2024-09-03 12:18:22 +0100

    Setting up the database

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
 FROM --platform=linux/amd64 debian:stable-slim
 
 RUN apt-get update && apt-get install -y ca-certificates
-#ENV PORT="8080"
-#ENV CONN_STRING="postgres://postgres:postgres@localhost:5432/blogator?sslmode=disable"
 
 ADD blog-aggregator /usr/bin/blog-aggregator
 
--- sql/schema/002_users_apikey.sql
+++ sql/schema/002_users_apikey.sql
@@ -1,7 +1,15 @@
 -- +goose Up
-ALTER TABLE users ADD COLUMN apikey VARCHAR(64) UNIQUE NOT NULL DEFAULT (
-    encode(sha256(random()::text::bytea), 'hex')
-);
+--ALTER TABLE users ADD COLUMN apikey VARCHAR(64) UNIQUE NOT NULL DEFAULT (
+--    encode(sha256(random()::text::bytea), 'hex')
+--);
+
+ALTER TABLE users ADD COLUMN apikey VARCHAR(64);
+
+UPDATE users
+SET apikey = lower(hex(randomblob(32)))
+WHERE apikey IS NULL;
+
+CREATE UNIQUE INDEX idx_users_apikey ON users(apikey);
 
 -- +goose Down
 ALTER TABLE users
